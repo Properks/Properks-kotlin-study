8712399222b298e24c4558ac54abfd950a3f40e1
Jeongmo Seo <wjdah1402@gmail.com>
2025-08-07 15:19:12 +0900

    반복문 학습

---
+++ kotlin-study/src/main/kotlin/org/jeongmo/kotlin/study/lec01/chapter11/Main.kt
@@ -0,0 +1,37 @@
+package org.jeongmo.kotlin.study.lec01.chapter11
+
+import org.jeongmo.kotlin.study.global.constants.PrintConstants
+
+fun main() {
+    var integer = 1
+    while (isUnder100(integer)) {
+        println("Integer: ${integer++}")
+    }
+
+    println(PrintConstants.LINE)
+
+    val range = 1..3
+    println("range: $range")
+    for (i in range) {
+        println("i: $i")
+    }
+
+    println(PrintConstants.LINE)
+
+    val range2 = 1 until 3
+    println("range2: $range2")
+    for (i in range2) {
+        println("i: $i")
+    }
+
+    println(PrintConstants.LINE)
+
+    val str = "String"
+    for (i in str) {
+        println("char: $i")
+    }
+
+
+}
+
+fun isUnder100(int : Int) = int < 100
